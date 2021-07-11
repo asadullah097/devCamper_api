6971d52b05585515dd013572aa4ec36878e15419
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2021-07-11 10:30:10 +0500

    mongoose and mongodb connection

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const dotenv = require("dotenv");
 
 const morgan=require('morgan');
 
+const colors=require('colors');
+
+const connectDB=require('./config/db')
+
 dotenv.config({ path: "./config/config.env" });
 
+//monogb connection
+connectDB();
 //routes files
 const bootcamps = require("./routes/bootcamps");
 
@@ -21,5 +27,11 @@ app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`server is runing in port ${PORT}`);
+  console.log(`server is runing on port ${PORT}`.yellow.bold);
+});
+
+//handle unhandle promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+  console.log(`Error :${err.message}`.red);
+  server.close(()=>process.exit(1))
 });
